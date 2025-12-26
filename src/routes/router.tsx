ed4f0ea281fecaeb96c18ea2c241c24d6c447f65
Nguyễn Thái Thuận <ntthuan1998z@gmail.com>
2025-12-26 15:39:11 +0700

    fix(import): use react-router

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import { authRoutes } from "./auth.route";
 
 const router = createBrowserRouter([
