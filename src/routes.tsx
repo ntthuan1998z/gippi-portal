55533b1bc62e9904df63a6e54a55ddbead3ff0bc
Nguyễn Thái Thuận <ntthuan1998z@gmail.com>
2025-12-26 14:15:12 +0700

    feat(authentication): add login page and setup react-router

--- src/routes.tsx
+++
@@ -1,7 +0,0 @@
-import { createBrowserRouter } from "react-router";
-import LoginPage from "./pages/Login";
-
-const routes = createBrowserRouter([{
-  path: "/login",
-  element: <LoginPage />,
-}]);
